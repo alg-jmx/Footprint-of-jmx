3bf622165076c1d11eb50ddc22e0f79011756174
jumingxing <jumingxing@puhuifinance.com>
2015-12-08 18:50:33 +0800

    html

--- Footprint-of-jmx.R
+++ Footprint-of-jmx.R
@@ -1,11 +1,13 @@
 library(REmap)
-data<-read.csv('D:\\Rworkspace\\Footprint-of-jmx\\Footprint-of-jmx\\Footprint-of-jmx.csv',encoding='utf-8')
+data<-read.csv('E:\\Rworkspace\\Footprint-of-jmx\\Footprint-of-jmx.csv',encoding='utf-8')
 set.seed(521)
+
 map<-remap(data,title = 'Footprint-of-jmx',subtitle = '15年的足迹',theme = get_theme('Dark'))
 plot(map)
 geoData2  = get_geo_position(unique(data[data==data]))
-map1<-remapB(markLineData = data,markPointData = data[,2],geoData=geoData2)
-remapB(markPointData = data[,2])
+map1<-remapB(markLineData = data,markPointData = data[,2],geoData=geoData2,
+             title = 'Footprint-of-jmx in recent years',subtitle = '足迹')
+remapB(markLineData = data, title = 'Footprint-of-jmx in recent years',subtitle = '足迹')
 plot(map1)
 
 geoData1  = get_geo_position(unique(demoC[demoC==demoC]))
