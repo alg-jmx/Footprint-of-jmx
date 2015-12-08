51c51826c44837d53b81ac90710cd72c45a42085
jumingxing <jumingxing@puhuifinance.com>
2015-12-08 18:38:39 +0800

    新增remapB函数

--- Footprint-of-jmx.R
+++ Footprint-of-jmx.R
@@ -1,5 +1,12 @@
 library(REmap)
-data<-read.csv('E:\\Rworkspace\\Footprint-of-jmx\\Footprint-of-jmx.csv')
+data<-read.csv('D:\\Rworkspace\\Footprint-of-jmx\\Footprint-of-jmx\\Footprint-of-jmx.csv',encoding='utf-8')
 set.seed(521)
 map<-remap(data,title = 'Footprint-of-jmx',subtitle = '15年的足迹',theme = get_theme('Dark'))
 plot(map)
+geoData2  = get_geo_position(unique(data[data==data]))
+map1<-remapB(markLineData = data,markPointData = data[,2],geoData=geoData2)
+remapB(markPointData = data[,2])
+plot(map1)
+
+geoData1  = get_geo_position(unique(demoC[demoC==demoC]))
+remapB(markLineData = demoC,geoData = geoData1)
